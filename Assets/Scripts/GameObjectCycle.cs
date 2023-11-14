086a8b828038e0152bf42d5949456ee19c88e77e
Thomas Graham <96675251+thomas-graham12@users.noreply.github.com>
2023-11-14 12:40:18 +1100

    Made some changes to the living room

---
+++ Assets/Scripts/GameObjectCycle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class GameObjectCycle : MonoBehaviour
+{
+    [SerializeField] private int numberInList;
+    [SerializeField] private List<GameObject> listOfGameObjects = new List<GameObject>();
+    
+
+    public void ChangeColor()
+    {
+        GameObjectsToAppear();
+    }
+
+    void GameObjectsToAppear()
+    {
+        if (numberInList >= 1)
+        {
+            for (int i = 0; i < listOfGameObjects.Count; i++)
+            {
+                listOfGameObjects[i].SetActive(false);
+            }
+
+            numberInList = 0;
+        }
+        else
+        {
+            for (int i = 0; i < listOfGameObjects.Count; i++)
+            {
+                listOfGameObjects[i].SetActive(true);
+            }
+
+            numberInList++;
+        }
+    }
+}
