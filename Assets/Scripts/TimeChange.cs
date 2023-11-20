e44349b24cc4df536faf7c06e683b05b88a12e56
Thomas Graham <96675251+thomas-graham12@users.noreply.github.com>
2023-11-20 17:51:50 +1100

    can turn on and off lamp

--- Assets/Scripts/TimeChange.cs
+++ Assets/Scripts/TimeChange.cs
@@ -12,7 +12,6 @@ public class TimeChange : MonoBehaviour
 
     public void ChangeColor()
     {
-        Debug.Log("hello");
         ColorChangeOnList();
         
         //if (numberInList == 3)
