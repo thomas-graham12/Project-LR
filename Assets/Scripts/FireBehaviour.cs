a8130279f9eeb597a1adf9ab98de1bbc0a900fdf
Thomas Graham <thomasgraham874@gmail.com>
2023-12-05 01:37:12 +1100

    Changed sound of rain, light switch, fire. Added music

---
+++ Assets/Scripts/FireBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBehaviour : MonoBehaviour
+{
+    [SerializeField] private int numberInList;
+    [SerializeField] private List<GameObject> listOfGameObjects = new List<GameObject>();
+    [SerializeField] private GameObject turnOnSoundGO;
+    [SerializeField] private GameObject turnOffSoundGO;
+    [SerializeField] private ParticleSystem _particleSystem;
+
+    private void Start()
+    {
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void ChangeGameObject()
+    {
+        GameObjectsToAppear();
+    }
+
+    void GameObjectsToAppear()
+    {
+        if (numberInList >= listOfGameObjects.Count)
+        {
+            for (int i = 0; i < listOfGameObjects.Count; i++)
+            {
+                var emission = _particleSystem.emission;
+                emission.enabled = false;
+                listOfGameObjects[i].SetActive(false);
+                
+                turnOnSoundGO.SetActive(false);
+                turnOffSoundGO.SetActive(true);
+            }
+
+            numberInList = 0;
+        }
+        else
+        {
+            for (int i = 0; i < listOfGameObjects.Count; i++)
+            {
+                var emission = _particleSystem.emission;
+                emission.enabled = true;
+                listOfGameObjects[i].SetActive(true);
+                
+                turnOffSoundGO.SetActive(false);
+                turnOnSoundGO.SetActive(true);
+            }
+
+            numberInList++;
+        }
+    }
+}
