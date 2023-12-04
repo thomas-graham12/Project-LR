d59cd08cf52d02d7e4e3b930bedafa17c72d13fb
rraidr <1023688@student.sae.edu.au>
2023-12-04 20:48:55 +1100

    Adjusted Couch model with new UV, testing multiple baked lighting methods

---
+++ Assets/TriggerBlueJay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerBlueJay : MonoBehaviour
+{
+
+    public PlayableDirector timeline;
+    
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+        StartCoroutine(DelayAnimation());
+    }
+
+    public IEnumerator DelayAnimation()
+    {
+        yield return new WaitForSeconds(5);
+        timeline.Play();
+
+    }
+
+}
