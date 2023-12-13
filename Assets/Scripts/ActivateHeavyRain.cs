7b3855a998a69ce4eaa044c67326569ebc720048
Thomas Graham <96675251+thomas-graham12@users.noreply.github.com>
2023-12-13 20:52:04 +1100

    Droplets now turn into rain after 12s

---
+++ Assets/Scripts/ActivateHeavyRain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateHeavyRain : MonoBehaviour
+{
+    [SerializeField] private GameObject heavyRain;
+
+    [SerializeField] private float timeUntilHeavyRain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartHeavyRain());
+    }
+
+    IEnumerator StartHeavyRain()
+    {
+        yield return new WaitForSeconds(timeUntilHeavyRain);
+        
+        heavyRain.SetActive(true);
+    }
+    
+}
