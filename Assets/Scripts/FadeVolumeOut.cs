db2c3e116fdd43393ba4169760e810d91a05f2fb
Thomas Graham <thomasgraham874@gmail.com>
2023-12-14 18:42:28 +1100

    Volume now fades out at the end

---
+++ Assets/Scripts/FadeVolumeOut.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeVolumeOut : MonoBehaviour
+{
+
+    [SerializeField] private float fadeOutDuration;
+    [SerializeField] private float timer = 0;
+    private float startingVolume;
+    [SerializeField] private float targetVolume = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingVolume = AudioListener.volume;
+        StartCoroutine(IFadeOutVolume());
+    }
+
+    IEnumerator IFadeOutVolume()
+    {
+        while (timer < fadeOutDuration)
+        {
+            timer += Time.deltaTime;
+            AudioListener.volume = Mathf.Lerp(startingVolume, targetVolume, timer / fadeOutDuration);
+            Debug.Log(AudioListener.volume);
+            yield return null;
+        }
+    }
+}
