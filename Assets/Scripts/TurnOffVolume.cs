33985fb3a30acc223a6b70cb03a7c030377bce81
Thomas Graham <thomasgraham874@gmail.com>
2023-12-14 16:46:34 +1100

    Trying to figure out how to turn down volume

---
+++ Assets/Scripts/TurnOffVolume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffVolume : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private float newVol = 0f;
+
+    private float timer = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+}
